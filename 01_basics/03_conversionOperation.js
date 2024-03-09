f73fc93921eaee50713bdce506593664e4ce488b
Ishan Trivedi <47516240+ishandtrivedi@users.noreply.github.com>
2024-03-09 06:49:02 +0000

    conversion of DataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,22 @@
+let score = "asdf";
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" or "asdfasdf" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+let age = 35;
+let stringAge = String(age);
+console.log(stringAge);
+console.log(typeof(stringAge));
